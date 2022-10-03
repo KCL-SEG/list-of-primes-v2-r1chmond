62f95a3f3c4cfeabc1b255e647c28ed4a8b49bed
richmond <richmond.bobie@outlook.com>
2022-10-03 22:17:33 +0100

    Complete prime numbers generator

--- primes.py
+++ primes.py
@@ -1,6 +1,24 @@
 """List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes < 1:
+        raise ValueError(f"Enter a value which is greater than 0")
     list = []
+    for i in range(2, 9223372036854775807):
+        if isPrime(i):
+            list.append(i)
+        if len(list) == number_of_primes:
+            break
     return list
+
+#check if a number is prime
+def isPrime(num):
+    if num == 2 or num == 3:
+        return True
+    elif num % 2 == 0 or num < 2 or num % 3 == 0:
+        return False
+    else:
+        for element in range(5, int((num ** (1/2))+1), 2):
+            if num % element == 0:
+                return False
+    return True
